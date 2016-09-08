49adbcff4865d0629df9cac25bf20dc21fa2146d
sivaaduri <sivaa@umich.edu>
2016-09-08 12:51:19 -0400

    Revert "start"
    This reverts commit b0130ea0a1fa4e9667cfa59ef96a1aca2189a636.

--- gps/get_assist_1.py
+++
@@ -1,8 +0,0 @@
-import requests
-r = requests.get("http://online-live1.services.u-blox.com/GetOnlineData.ashx?token=5pEAcGuLwk-Nw3rqXOmqzA;gnss=gps;datatype=eph,alm;format=aid;lon=-83.726285;lat=42.231925;pacc=10;filteronpos")
-filess=open("GPS_assist.txt",'wb')
-filess.write(r.content)
-filess.close()
-print r.status_code
-print r.headers
-print r.content
